e29446aa20e2900be5cdf599248d3dfcc3cf8fa0
wwilliamwai <wongwilliam102@gmail.com>
2024-08-16 03:30:55 -0700

    completed working connect4game with message

---
+++ connectfour/src/App.jsx
@@ -0,0 +1,139 @@
+import { useState } from 'react';
+import './App.css';
+
+function Circle({onClick, color}) {
+  return <div onClick={onClick} className="circle" style={{backgroundColor: color}}/>
+} 
+function Square({onClick, color}) { 
+  return <div className="square" >
+    <Circle onClick={onClick} color={color}/>
+  </div>
+}
+function Board({squares, setSquares, isRedTurn, setRedTurn, isGameOver, setGameOver}) {
+  function handleClick(rowIndex, columnIndex) {
+    if (isGameOver) {
+      return;
+    }
+    if (rowIndex + 1 === squares.length ) {  
+      if (squares[rowIndex][columnIndex] !== 'whitesmoke') {
+        return;
+      }
+    } else if (squares[rowIndex + 1][columnIndex] === 'whitesmoke' || squares[rowIndex][columnIndex] !== 'whitesmoke') {
+      return;
+    }
+    const copySquares = squares.map((row) => row.slice());
+    copySquares[rowIndex][columnIndex] = isRedTurn ? 'red' : 'yellow'; 
+
+    if (checkGameOver(copySquares)) {
+      setGameOver(true);
+    }
+    setSquares(copySquares);
+    setRedTurn(!isRedTurn);
+    }
+    return (
+      <> 
+        {squares.map((row, rowIndex) => 
+        (
+          <div key={rowIndex} className="row">
+            {row.map((square, columnIndex) => 
+            <Square key={rowIndex.toString() + columnIndex.toString()} class="square" onClick={() => handleClick(rowIndex, columnIndex)} color={squares[rowIndex][columnIndex]}/>
+            )}
+          </div>
+        )
+        )}
+      </>
+  )
+} 
+export default function Game() {
+  const [squares, setSquares] = useState(Array(6).fill().map(() => Array(7).fill('whitesmoke')));
+  const [isRedTurn, setRedTurn] = useState(true);
+  const [isGameOver, setGameOver] = useState(false);
+
+  return (
+    <>
+    <div class="game">
+      {isGameOver ? isRedTurn ? <h1 className="game-over yellow">Yellow Wins!</h1> : <h1 className="game-over red">Red Wins!</h1> : "" }
+      {!isGameOver ? isRedTurn ? <h1 className="next-player">Next Player: <span className="red">Red</span></h1> : <h1 className="next-player">Next Player: <span className="yellow">Yellow</span></h1> : ""}
+      <div className="board">
+        <Board squares={squares} setSquares={setSquares} isRedTurn={isRedTurn} setRedTurn={setRedTurn} isGameOver={isGameOver} setGameOver={setGameOver}/>
+      </div>
+    </div>
+    </>
+  )
+}
+function checkGameOver(squares) {
+  for(let row = 0; row < squares.length; row++) {
+    for(let col = 0; col < squares[0].length; col++) {
+      if (isVerticalFour(squares, row, col) || isHorizontalFour(squares, row, col) || isDiagonalFour(squares, row, col)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+function isVerticalFour(squares, targetRow, targetCol) {
+  if (squares[targetRow][targetCol] === 'whitesmoke') {
+    return false;
+  }
+  let count = 0;
+  // check vertically if count === 4
+  for (let row = targetRow - 3; row <= targetRow + 3; row++) {
+    if (row >= 0 && row < squares.length && squares[row][targetCol] === squares[targetRow][targetCol]) {
+      count++;
+      if (count === 4) {
+        return true;
+      }
+    } else {
+    count = 0;
+    }
+  }
+  return false;
+}
+function isHorizontalFour(squares, targetRow, targetCol) {
+  if (squares[targetRow][targetCol] === 'whitesmoke') {
+    return false;
+  } 
+  let count = 0;
+  // check horizontally if count == 4
+  for (let col = targetCol - 3; col <= targetCol + 3; col++) {
+    if (col >= 0 && col < squares[0].length && squares[targetRow][col] === squares[targetRow][targetCol]) {
+      count++;
+      if (count === 4) {
+        return true;
+      }
+    } else {
+      count = 0;
+    }
+  }
+  return false;
+}
+function isDiagonalFour(squares, targetRow, targetCol) {
+  if (squares[targetRow][targetCol] === 'whitesmoke') {
+    return false;
+  }
+  let count = 0;
+  // check topleft diagonal if count === 4
+  for (let i = -3; i <= 3; i++) {
+    if (targetRow + i >= 0 && targetCol + i >= 0 && targetRow + i < squares.length && targetCol + i < squares[0].length 
+      && squares[targetRow + i][targetCol + i] === squares[targetRow][targetCol] ) {
+        count++;
+        if (count === 4) {
+          return true;
+        }
+    } else {
+      count = 0;
+    }
+  }
+  for (let i = -3; i <= 3; i++) {
+    if (targetRow - i >= 0 && targetCol + i >= 0 && targetRow - i < squares.length && targetCol + i < squares[0].length
+      && squares[targetRow - i][targetCol + i] === squares[targetRow][targetCol]) {
+        count++;
+        if (count === 4) {
+          return true;
+        }
+      } else {
+        count = 0;
+      }
+  }
+  return false;
+}
---
+++ connectfour/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Game from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Game />
+  </StrictMode>,
+)
